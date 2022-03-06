481fbe907f3d530851d74dd2464b74cf78ede40b
kishan831 <76108383+kishan831@users.noreply.github.com>
2022-03-06 09:52:57 +0530

    updated player scripts

--- Script/Player.cs
+++ Script/Player.cs
@@ -1,30 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour {
 
+    //Some Value for inspector panel    
     [SerializeField] public float MoveSpeed,JumpForce,Runspeed;
     private Rigidbody2D rb;
-
+     
+    //Appling LayerMask 
     [SerializeField] private LayerMask GroundLayer;
 
-    [SerializeField] private Transform groundCheckpos;
+   //Checking For Ground Pos.
+   [SerializeField] private Transform groundCheckpos;
 
     private BoxCollider2D boxcol2d;
 
     private Animator anim;
 
+    public ScoreController scoreController;
+    public GameOverController gameOverController;
+
     private void Start() {
-        rb = GetComponent<Rigidbody2D>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         boxcol2d = GetComponent<BoxCollider2D>();
     }
 
+
+    //Doing Actions For Every Frame
     private void Update() {
 
         Move();
-
         HandleJump();
 
         if(Input.GetKeyDown(KeyCode.C)) {
@@ -32,23 +40,21 @@ public class Player : MonoBehaviour {
         }
 
     }
-
-    void SetJumpAnimation() { 
-            anim.SetTrigger("isJump");
-    }
-
-    void SetCrouchAnimation() {
-            anim.SetTrigger("isCrouch");       
-    }
-
+    
+    //Actions For Movement Of Player
     private void Move() {
 
+        float moveX = 0f;
+
         var movement = Input.GetAxisRaw("Horizontal");
-        transform.position += new Vector3(movement,0,0) * Time.deltaTime * MoveSpeed;
+        Vector3 position = transform.position;
+        position.x += movement * MoveSpeed * Time.deltaTime;
+        transform.position = position;
+        
 
         if(Input.GetKey(KeyCode.D) && movement > 0) {
-
-             MoveSpeed = Runspeed;
+             
+             moveX = MoveSpeed;
              Vector3 temp = transform.localScale;
              temp.x = 1f;
              transform.localScale = temp;
@@ -57,22 +63,25 @@ public class Player : MonoBehaviour {
 
         else if( movement < 0 && Input.GetKey(KeyCode.A)) {
 
-            MoveSpeed = Runspeed;
+            moveX = -MoveSpeed;
             Vector3 temp = transform.localScale;
             temp.x = -1f;
             transform.localScale = temp;
             anim.SetBool("isRun",true);
         }
 
-        else {
+        else  {
             anim.SetBool("isRun",false);
             }
     }
 
+    //Checking Player Is On The Ground or Not
     bool IsGrounded() {
-        return Physics2D.Raycast(groundCheckpos.position,Vector2.down,0.1f,GroundLayer);
+        return Physics2D.OverlapCircle(groundCheckpos.position,0.2f,GroundLayer);
     }
 
+
+    //setting Up Jump
     void HandleJump() {
         
         if(Input.GetKeyDown(KeyCode.Space) && IsGrounded()) {
@@ -82,6 +91,28 @@ public class Player : MonoBehaviour {
 
         }
 
+    }
+
+    //Different Animation As per Need. 
+    void SetJumpAnimation() {
+        anim.SetTrigger("isJump");
+    }
+
+    void SetCrouchAnimation() {
+            anim.SetTrigger("isCrouch");       
+    }
+
+    //collectibles
+    public void Collectibles() {
+        Debug.Log("Key Collected");
+        scoreController.IncScore(2);
+    }
+   
+    //Taking Some Damage on player
+    public void AttackOnPlayer() {
+        Debug.Log("Player Is Being Attacked");
+        gameOverController.PlayerDied();
+    
     }
             
 
